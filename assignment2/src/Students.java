10b563a02f976d162d7bdaf4d207f2926088402b
Beast mode <davis199994@yahoo.com>
2017-01-29 17:34:54 -0500

    reworked the git configuration

---
+++ assignment2/src/Students.java
@@ -0,0 +1,19 @@
+/*
+Donald Davis
+IT 2660 Data Structures & Algorithms CRN 13907
+Due February 9, Spring 2017
+Chapter 2 Exercise 19,20 + 21
+ */
+public class Students {
+    public static void displayClassInfo()   {
+        System.out.println("Donald Davis Assignment 1");
+        System.out.println("IT 2660 Data Structures and Algorithms, CRN 13907,");
+        System.out.println("Due February 9, 2017     Spring 2017");
+        System.out.println("Chapter 2 Exercise 19,20 + 21");
+    }
+
+    public static void main(String[] args)  {
+        displayClassInfo();
+
+    }
+}
