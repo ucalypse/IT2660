8440ef0a727e8f6e8c38cad72cec1a8a9b97e046
Beast mode <davis199994@yahoo.com>
2017-01-30 22:29:21 -0500

    stuck non building project

--- Assignment2/src/SortedArray.java
+++ Assignment2/src/SortedArray.java
@@ -2,37 +2,35 @@
 Donald Davis
 IT 2660 Data Structures & Algorithms CRN 13907
 Due February 9, Spring 2017
-Chapter 2 Exercise 19
+Chapter 2 Exercise 20
  */
 import java.util.Scanner;
 
-public class StudentListings {
+public class SortedArray {
     String name;
     String id;
     String gpa;
     Scanner input = new Scanner(System.in);
 
-    public StudentListings(String n, String num, String a){
+    public SortedArray(String n, String num, String a){
         name = n;
         id = num;
         gpa = a;
     }
-    public StudentListings(){
+    public SortedArray(){
 
     }
+
     public String toString()    {
         return("Name is " + name + "\nG.P.A. is " + gpa + "\nStudent ID is " + id + "\n");
     }
-    public StudentListings deepCopy(){
-        StudentListings clone = new StudentListings(name, id, gpa);
+    public SortedArray deepCopy(){
+        SortedArray clone = new SortedArray(name, id, gpa);
         return clone;
     }
-    public int compareTo(String targetKey){
-        return (name.compareTo(targetKey));
-    }
 
-    public StudentListings getInput()   {
-        StudentListings student = new StudentListings();
+    public SortedArray getInput()   {
+        SortedArray student = new SortedArray();
         System.out.println("Enter the Student Name");
         student.name = input.nextLine();
         System.out.println("Enter the StudentID");
@@ -48,20 +46,31 @@ public class StudentListings {
         System.out.println("Due February 9, 2017     Spring 2017");
         System.out.println("Chapter 2 Exercise 19,20 + 21" + "\n");
     }
+    //Handcoded SortedArray method
+    public String[] sortArrayFetch(int high, String targetKey)  {
+        String[] inputArray = new String[high];
+        int low = 0;
+        int maximum = high - 1;
+        int i = maximum / 2;
+        while(targetKey != inputArray[i]){
+            if(targetKey < inputArray[i] && maximum != low) {
+                maximum = i - 1;
+            }
+            else    {
+                low = i +1;
+            }
+            i = (low + maximum) / 2;
+        }
+        return inputArray[i];
+
+    }
 
     public static void main(String[] args)  {
-        StudentListings newStudent;
-        StudentListings copyOfnewStudent;
-        StudentListings studentListings = new StudentListings();
+        SortedArray sortedStudent;
+        SortedArray copyOfnewStudent;
+        SortedArray sortedArray = new SortedArray();
 
         displayClassInfo();
-
-         newStudent = studentListings.getInput();
-         copyOfnewStudent = studentListings.deepCopy();
-         System.out.println(copyOfnewStudent.toString());
-         int compareToInfo = studentListings.compareTo("Mark");
-         System.out.println(compareToInfo);
-         System.out.println(newStudent.toString());
 
     }
 }
