46a85fda000e28aabfabd609d931729dd64d973b
Donald Davis <donald.m.davis@sherwin.com>
2017-02-17 14:48:26 -0500

    wah

--- Assignment3/src/CustomQueue.java
+++ Assignment3/src/CustomQueue.java
@@ -22,26 +22,17 @@ public class CustomQueue {
 
         Scanner input = new Scanner(System.in);
         CustomQueue customQueue = new CustomQueue();
-        Stack stack = new Stack();
+
         displayClassInfo();
         System.out.println("Enter the expression in post-fixed notation separated by a space");
         String sampleString = input.nextLine();
+        Stack stack = new Stack(sampleString.length());
         String [] newValues = customQueue.convertToStringArray(sampleString);
         int[] result = customQueue.convertToIntegers(newValues);
-        for (int item: result) {
-            System.out.println(item);
-
+        for (int i=0; i< result.length; i++)    {
+            System.out.println(result[i]);
         }
-        System.out.println(result.toString());
         stack.push(sampleString);
-        stack.showAll();
-    }
-    public int[] processLine(String strings) {
-        int[] intArray = new int[strings.length()];
-        for(int i=0;i<strings.length();i++) {
-            intArray[i] = Integer.parseInt(String.valueOf(strings.charAt(i)));
-        }
-        return intArray;
     }
 
     public int[] convertToIntegers(String[] tokens) {
@@ -49,7 +40,8 @@ public class CustomQueue {
         int j = 0;
         int[] newArray = new int[tokens.length];
         int returnValue;
-        while(i < tokens.length - 1)    {
+        while(i < tokens.length)    {
+
             if (tokens[i] == "+") {
                 int result = Integer.parseInt(tokens[i-1]) + Integer.parseInt(tokens[i-2]);
                 newArray[j]=result;
@@ -70,6 +62,7 @@ public class CustomQueue {
                 newArray[j]=result;
                 j++;
             }
+            newArray[i] = Integer.parseInt(tokens[i]);
             i++;
         }
         return newArray;
@@ -77,9 +70,10 @@ public class CustomQueue {
 
     public String[] convertToStringArray(String tokens) {
        int i = 0;
-        String[] newArray = new String[tokens.length()];
+
         parse = new StringTokenizer(tokens," ");
 
+        String[] newArray = new String[parse.countTokens()];
         while(parse.hasMoreTokens())    {
                newArray[i] = parse.nextToken();
                 i++;
