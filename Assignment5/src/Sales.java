3bb88bd675cf3629afe0ecbcefeff0b0a02ddf4b
Beast mode <davis199994@yahoo.com>
2017-03-29 23:50:06 -0400

    blah

--- Assignment5/src/Sales.java
+++ Assignment5/src/Sales.java
@@ -17,7 +17,6 @@ public class Sales {
     }
 
     public Ticket fetchTicket(int seatNumber)    {
-        int index;
         for(int i = 0; i < keys.length; i++)   {
             if(keys[i] == seatNumber)   {
                 return tickets[seatNumber];
